ed32e3485f0266a740ed3cd19d71ac9ab9dbd2bb
Todd Fleeman <rtoddf@comcast.net>
2014-08-03 10:53:53 -0400

    started list learning

---
+++ python/learning/lists/list01.py
@@ -0,0 +1,48 @@
+list1 = ['physics', 'chemistry', 1997, 2000];
+list2 = [1, 2, 3, 4, 5, 6, 7 ];
+
+# list values can be accessed by their indices
+
+print 'the list values can be accessed with indices: ', list1[1]
+print 'from list 2: ', list2[4]
+
+year_list = [2000, 2002, 2003, 2004, 2005]
+print 'year list: ', year_list
+
+# you can update the list by calling the index and the new value
+year_list[1] = 2001
+print 'year list after: ', year_list
+
+# use del list[i] to deleted an item
+del year_list[0]
+print 'after delete: ', year_list
+
+'''
+Basic operations
+Lists respond to the + and * operators much like strings;
+they mean concatenation and repetition here too,
+except that the result is a new list, not a string.
+'''
+
+list_nums1 = [1, 2, 12, 24, 98, 102]
+list_nums2 = [15, 24, 76, 98, 1002]
+
+# length
+print 'length: ', len(list_nums1)
+
+# concatenation
+list_add = list_nums1 + list_nums2
+print 'list_add: ', list_add
+
+# repetition
+list_mult = ['help']
+print list_mult*4
+
+# membership
+print 24 in list_nums1
+
+bobs = ['Bobby', 'Bob', 'Robert', 'Bobbie', 'Bobberino']
+print 'whole list: ', bobs
+print 'from the last: ', bobs[-1]
+print 'starting with: ', bobs[1:]
+print 'range, up to but not including: ', bobs[1:3]
---
+++ python/learning/lists/list02.py
@@ -0,0 +1,55 @@
+list1 = [1, 2, 12, 24, 98, 102]
+list2 = [15, 24, 76, 98, 1002]
+
+'cmp compares elements of both lists'
+
+print cmp(list1, list2)
+
+'''
+FUNCTIONS - len, max, min
+'''
+
+print 'max value: ', max(list1)
+print 'min value: ', min(list1)
+
+
+'''
+METHODS - append, extend, count, index, insert, remove, sort, reverse
+remove only removes the first instance of the obj
+
+pop, removes and returns the last item in the list
+
+? - reverse
+'''
+
+list2.append(98)
+print 'append: ', list2
+print 'count: ', list2.count(98)
+
+list2.extend([45, 45])
+print 'extend list2: ', list2
+
+print 'index: ', list2.index(76)
+# print 'index: ', list2.index(103)
+
+list2.insert(2, 49)
+print 'insert list2: ', list2
+
+# print 'pop: ', list2.pop()
+# print list2
+
+list2.remove(45)
+print 'remove: ', list2
+
+list5 = ['bob', 'cat', 'dog', 'apple']
+print list5
+
+list5.reverse()
+print 'reverse list5: ', list5
+list5.sort()
+print 'reverse list5: ', list5
+
+a = [66.25, 333, 333, 1, 1234.5]
+a.reverse()
+print a
+
