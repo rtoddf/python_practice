21f2eeef772d8ec6f245a8e91014312fbc1a7ddb
Todd Fleeman <rtoddf@comcast.net>
2014-08-10 12:59:45 -0400

    more pil practice

--- pil/retrieve.py
+++ pil/retrieve.py
@@ -8,13 +8,13 @@ image_base_url = 'http://tube.tmsimg.com/h10/AllPhotos/'
 photos = [9248657, 185113, 9994933, 10426177, 3563021, 186700, 185554, 10291619, 9263605, 10475136, 9248657, 
 9991822, 9895254, 10106164, 10063537, 9957538, 10086642, 10024445, 10372543, 10102709, 10367302]
 
-# def get_photo(phoot_id):
-#     image_url = image_base_url + phoot_id + '/p' + phoot_id + '_i_h11_aa.jpg'
-#     image_dest = photo_directory + phoot_id + '.jpg'
-#     urllib.urlretrieve(image_url, image_dest)
+def get_photo(phoot_id):
+    image_url = image_base_url + phoot_id + '/p' + phoot_id + '_i_h11_aa.jpg'
+    image_dest = photo_directory + phoot_id + '.jpg'
+    urllib.urlretrieve(image_url, image_dest)
 
-# for photo in photos:
-#     get_photo(str(photo))
+for photo in photos:
+    get_photo(str(photo))
 
 # The format attribute identifies the source of an image. If the image was not read from a file,
 # it is set to None. The size attribute is a 2-tuple containing width and height (in pixels).
@@ -24,19 +24,19 @@ photos = [9248657, 185113, 9994933, 10426177, 3563021, 186700, 185554, 10291619,
 
 # If the file cannot be opened, an IOError exception is raised.
 
-image = Image.open('images/10475136.jpg')
-thumbnail_directory = 'images/thumbnails/'
-size = 300, 170
+# image = Image.open('images/10475136.jpg')
+# thumbnail_directory = 'images/thumbnails/'
+# size = 300, 170
 
 # for infile in sys.argv[1:]:
 # 	print 'infile: ', infile
 
-outfile = 'thumbnails/image.jpg'
+# outfile = 'thumbnails/image.jpg'
 # image.thumbnail(size)
-out = image.resize((300, 170))
-out.save(outfile, 'JPEG')
+# out = image.resize((300, 170))
+# out.save(outfile, 'JPEG')
 
-print 'out.size: ', out.size
+# print 'out.size: ', out.size
 
 # print 'image.format: ', image.format
 # print 'image.size: ', image.size
---
+++ pil/sprites.py
@@ -0,0 +1,97 @@
+import urllib
+import os, sys
+from PIL import Image
+
+image_urls_base = 'http://tube.tmsimg.com/'
+image_directory = 'images/thumbnails/'
+
+settings = {
+    'width': 300,
+    'height': 170,
+    'num_vertical': 4,
+    'num_horizontal': 5,
+    'total_images': 20
+}
+
+image_urls = ['h11/AllPhotos/185113/p185113_i_h11_aa.jpg',
+'h11/AllPhotos/9994933/p9994933_i_h11_aa.jpg',
+'h11/AllPhotos/10426177/p10426177_i_h11_aa.jpg',
+'h11/AllPhotos/3563021/p3563021_i_h11_aa.jpg',
+'h11/AllPhotos/186700/p186700_i_h11_aa.jpg',
+'h11/AllPhotos/185554/p185554_i_h11_aa.jpg',
+'h11/AllPhotos/10291619/p10291619_i_h11_aa.jpg',
+'h11/AllPhotos/9263605/p9263605_i_h11_ab.jpg',
+'h11/AllPhotos/10475136/p10475136_i_h11_aa.jpg',
+'h10/AllPhotos/9248657/p9248657_i_h11_aa.jpg',
+'h10/AllPhotos/9991822/p9991822_i_h11_aa.jpg',
+'h10/AllPhotos/9895254/p9895254_i_h11_aa.jpg',
+'h10/AllPhotos/10106164/p10106164_i_h11_aa.jpg',
+'h10/AllPhotos/10063537/p10063537_i_h11_aa.jpg',
+'h10/AllPhotos/9957538/p9957538_i_h11_aa.jpg',
+'h10/AllPhotos/10086642/p10086642_i_h11_aa.jpg',
+'h10/AllPhotos/10024445/p10024445_i_h11_aa.jpg',
+'h10/AllPhotos/10372543/p10372543_i_h11_aa.jpg',
+'h10/AllPhotos/10102709/p10102709_i_h10_aa.jpg',
+'h10/AllPhotos/10367302/p10367302_i_h10_aa.jpg']
+
+# size = 300, 170
+
+def get_imlist(path):
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg')]
+
+# download all images and save them to a thumbnails dir
+for im in image_urls:
+    image_url = image_urls_base + str(im)
+    # file, ext = os.path.splitext(im)
+    image_dest =  image_directory + im.rsplit('/', 1)[-1]
+    # urllib.urlretrieve(image_url, image_dest)
+
+# read the images from the dir - returns a list
+image_list = get_imlist(image_directory)
+
+sprite_dimensions = (settings['num_horizontal'] * settings['width'], settings['num_vertical'] * settings['height'])
+sprite_image = Image.new('RGB', sprite_dimensions, 'white')
+
+# for image in image_list:
+#     os.remove(image)
+
+the_images = []
+
+for image in image_list:
+    im = Image.open(image)
+    im = im.resize((settings['width'], settings['height']))
+    the_images.append(im)
+
+# create a thumbnail for each image
+for i, image in enumerate(the_images):
+    for j in range(5):
+        if i < j:
+            horizontal_point =  j * settings['width']
+            vertical_point = 0
+            print (horizontal_point, vertical_point)
+            sprite_image.paste(image, (horizontal_point, vertical_point))
+    # elif i > settings['num_horizontal'] and i < (settings['num_horizontal'] * 2):
+    #     j = 1
+    #     horizontal_point = 1
+    #     sprite_image.paste(out, (settings['width'] * j, horizontal_point * settings['height']))
+    #     j += 1
+    # elif i > (settings['num_horizontal'] * 2) and i < (settings['num_horizontal'] * 3):
+    #     j = 1
+    #     horizontal_point = 2
+    #     sprite_image.paste(out, (settings['width'] * j, horizontal_point * settings['height']))
+    #     j += 1
+    # elif i > (settings['num_horizontal'] * 3) and i < (settings['num_horizontal'] * 4):
+    #     j = 1
+    #     horizontal_point = 3
+    #     sprite_image.paste(out, (settings['width'] * j, horizontal_point * settings['height']))
+    #     j += 1
+    
+    
+sprite_image.show()
+
+
+
+
+
+
+
